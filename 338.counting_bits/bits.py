4ebf68195b687560a81fd9d8d291f55e0e0aae8d
vlom <vlomplusplus@gmail.com>
2025-11-23 21:34:48 +0200

    Counting bits

---
+++ 338.counting_bits/bits.py
@@ -0,0 +1,49 @@
+# 338. Counting bits
+# Topics: 'Bit Manipulation', 'Dynamic Programming'
+
+# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+# Example 1:
+
+# Input: n = 2
+# Output: [0,1,1]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+
+# Example 2:
+
+# Input: n = 5
+# Output: [0,1,1,2,1,2]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# 3 --> 11
+# 4 --> 100
+# 5 --> 101
+
+ 
+
+# Constraints:
+
+#     0 <= n <= 105
+
+from typing import List
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+
+        for i in range (n+1):
+            count = 0
+            while i > 0:
+                if i & 1 == 1:
+                    count+=1
+                i = i >> 1
+            res.append(count)
+
+        return res
