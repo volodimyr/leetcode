a6dcc18a25ba0376a73e1e253f70c57698d69c71
vlom <vlomplusplus@gmail.com>
2026-01-30 14:02:31 +0200

    Coin Change

--- 322.coin_change/coin.py
+++ 322.coin_change/coin.py
@@ -34,27 +34,58 @@
 #     1 <= coins[i] <= 231 - 1
 #     0 <= amount <= 104
 
+import math
 from typing import List
 
-
+# bottom-up
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
-        
-        cache = {}
-        def helper(amount):
-            if amount == 0:
-                return 0
-            if amount in cache:
-                return cache[amount]
-            
-            res = 1e9
-            for c in coins:
-                if amount-c >= 0:
-                    res = min(res, 1+helper(amount-c))
-            cache[amount] = res
-            return res
+        dp = [math.inf] * (amount+1)
+        dp[0] = 0
 
-        res = helper(amount)
-        return -1 if res == 1e9 else res
+        for a in range(amount+1):
+            for c in coins:
+                if c <= a:
+                    dp[a] = min(dp[a], 1+dp[a-c])
+        
+        return dp[amount] if dp[amount] != math.inf else -1
+
+        # if amount == 0:
+        #     return 0
+        # min_steps = math.inf
+        # memo = {}
+        # def coin(n):
+        #     if n == 0:
+        #         return 0
+        #     if n in memo:
+        #         return memo[n]
+        #     min_steps = math.inf
+        #     for c in coins:
+        #         if n-c >= 0:
+        #             min_steps = min(1+coin(n-c), min_steps)
+        #     memo[n] = min_steps
+        #     return memo[n]
+        # res = coin(amount)
+        # return -1 if res == math.inf else res
+
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         if amount == 0:
+#             return 0
+        
+#         cache = {}
+#         def helper(amount):
+#             if amount == 0:
+#                 return 0
+#             if amount in cache:
+#                 return cache[amount]
+            
+#             res = 1e9
+#             for c in coins:
+#                 if amount-c >= 0:
+#                     res = min(res, 1+helper(amount-c))
+#             cache[amount] = res
+#             return res
+
+#         res = helper(amount)
+#         return -1 if res == 1e9 else res
